b1345277d18c348cb8cee3e5432f1f07f1e3317e
Eduardo Araújo <99681876+eduardoaraujo006@users.noreply.github.com>
2022-11-29 09:09:02 -0300

    Add files via upload

---
+++ IMC_Func.php
@@ -0,0 +1,20 @@
+<?php
+# Função que calcula o IMC #
+function IMC($peso, $altura){
+    $IMC = $peso / ($altura ** 2);
+    $IMC = round($IMC, 2);
+    return $IMC;
+}
+# Função que calcula a meta de peso (18.69 - 24.99) #
+function PesoIdeal($altura, $IMC){
+    if ($IMC < 19)
+    while ($IMC >= 19 and $IMC <= 25){
+        $pesoideal = $IMC * ($altura ** 2);
+        $pesoideal = round($pesoideal, 2);
+        echo $pesoideal." | ";
+        echo $IMC." | ";
+        echo nl2br("\n");
+        $IMC = $IMC + 1;
+    }
+}
+?>
---
+++ Program.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>
+    <form action="" method="post">
+        Digite seu peso: <input type="int" name="peso"/>
+        Digite sua altura: <input type="int" name="altura"/><br><br>
+        <button type="submit" name="bnt">Enviar</button><br><br>
+    </form>
+    <title>IMC</title>
+</head>
+</html>
+
+<?php
+include ("IMC_Func.php");
+$peso = $_REQUEST["peso"];
+$altura = $_REQUEST["altura"];
+
+if (isset($_REQUEST["bnt"])){
+    echo ("Seu IMC é: ".IMC($peso, $altura));
+    echo nl2br("\n");
+    echo nl2br("\n");
+    echo PesoIdeal($altura, IMC($peso, $altura));
+}
+?>
